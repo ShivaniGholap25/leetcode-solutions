045ae5e09091c94693e9371e9a293fd9904a9812
Shivani Gholap <shivanigholap9021@gmail.com>
2026-07-30 23:41:28 +0530

    Time: 43 ms (18.08%) | Memory: 16.1 MB (22.94%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.py
+++ 485-max-consecutive-ones/max-consecutive-ones.py
@@ -1,16 +1,14 @@
 class Solution(object):
     def findMaxConsecutiveOnes(self, nums):
-        count=0
-        max_count=0
-
-        for i in range(0 , len(nums)):
-            if nums[i] ==1:
+        count = 0
+        answer = 0
+        for i in range(len(nums)):
+            if nums[i] == 1:
                 count += 1
+                answer = max(answer,count)
             else:
-                max_count = max(max_count, count)
                 count = 0
-
-        return  max(count , max_count)        
+        return answer      
 
     
         
